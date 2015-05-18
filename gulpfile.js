acda9a2ba4b5395200765da62e0ee3f5be4a72e7
Mark Otto <markdotto@gmail.com>
2015-05-17 17:44:20 -0700

    break into smaller files, use inline source to compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-var gulp    = require('gulp');
-var htmlmin = require('gulp-htmlmin');
+var gulp         = require('gulp'),
+    htmlmin      = require('gulp-htmlmin'),
+    inlinesource = require('gulp-inline-source');
+
+gulp.task('inlinesource', function () {
+  return gulp.src('./src/github-btn.html')
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./github-btn.html'));
+});
 
 // Minify HTML
 gulp.task('minify', function() {
-  gulp.src('./src/github-btn.html')
+  gulp.src('./github-btn.html')
     .pipe(htmlmin({
       collapseWhitespace: true,
       minifyCSS: true,
       minifyJS: true,
       removeOptionalTags: true
     }))
-    .pipe(gulp.dest('./'))
+    // .pipe(gulp.dest('./'))
 });
 
 // Default Task
